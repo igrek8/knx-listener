18c1baa7674cc5265e6f5921c8f4dc94ce301075
crabicode <se.meridian@gmail.com>
2017-01-21 01:04:03 +0500

    fix typing

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+__export(require("./utils/index"));
+var bus_listener_1 = require("./bus-listener");
+exports.BusListener = bus_listener_1.BusListener;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7OztBQUFBLG1DQUE4QjtBQUM5QiwrQ0FBNkM7QUFBcEMscUNBQUEsV0FBVyxDQUFBIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0ICogZnJvbSAnLi91dGlscy9pbmRleCc7XG5leHBvcnQgeyBCdXNMaXN0ZW5lciB9IGZyb20gJy4vYnVzLWxpc3RlbmVyJztcbiJdfQ==
