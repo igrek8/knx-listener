b45886165ddd65303151e8158062e1e048063c86
crabicode <se.meridian@gmail.com>
2017-01-21 01:04:03 +0500

    fix typings

--- index.js
+++
@@ -1,8 +0,0 @@
-// tslint:disable:eofline
-// tslint:disable:quotemark
-
-const BusListener = require('./dist/bus-listener').BusListener;
-const utils = require('./dist/utils/index');
-
-exports.BusListener = BusListener;
-exports.utils = utils;
